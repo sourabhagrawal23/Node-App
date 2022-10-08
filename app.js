f781c240f7584258c538704cdf1401678f25a071
sourabh agrawal <sourabhkhs23@gmail.com>
2022-10-08 23:12:50 +0530

    Parsing request bodies and understanding even driven code

--- app.js
+++ app.js
@@ -1,9 +1,40 @@
 const http = require('http');
+const fs = require('fs');
 
 //this function will run for every request that reaches our server
 function rqListener(req,res) {
-    console.log(req.url, req.method, req.headers);
+    // console.log(req.url, req.method, req.headers);
     // process.exit();
+    const url = req.url;
+    const method = req.method;
+
+    if(url === '/')
+    {
+        res.write("<HTML>");
+        res.write("<Head><Title>Enter Message</Title></Head>");
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"></form></body>');
+        res.write("</HTML>");
+        return res.end();
+    }    
+
+    if(url === '/message' && method === 'POST')
+    {
+        const body = [];
+        req.on('data',(chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+        return req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt',message);
+            res.statusCode = 302;
+            res.setHeader('Location','/');
+            return res.end();
+        });
+        
+    }
+
     res.setHeader('Content-Type','text/html');
     res.write("<HTML>");
     res.write("<Head><Title></Title></Head>");
