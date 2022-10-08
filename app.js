7bb6b5197c52bfb80c21925dab70778a75388f9f
sourabh agrawal <sourabhkhs23@gmail.com>
2022-10-08 19:27:04 +0530

    Sending Responses - end

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const http = require('http');
 function rqListener(req,res) {
     console.log(req.url, req.method, req.headers);
     // process.exit();
+    res.setHeader('Content-Type','text/html');
+    res.write("<HTML>");
+    res.write("<Head><Title></Title></Head>");
+    res.write("<body><h1>Hello from my Node.js server!</h1></body>");
+    res.write("</HTML>");
+    res.end();
 }
 
 const server = http.createServer(rqListener);
